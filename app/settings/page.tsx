a43af08734a3c6d20a6044a04c5e8af2ee6ef8a2
Home Desktop <kucera1114@gmail.com>
2025-02-06 23:56:08 +0100

    css settings page fix

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -3,26 +3,27 @@ import { useContext, useEffect, useState } from "react"
 import "./style.css"
 import { UserContext } from "../Contexts/UserContext"
 import { Urls } from "../Contexts/UrlsExport"
+import { div } from "framer-motion/client"
 
 export default function Settings() {
     const user = useContext(UserContext)
     console.log("Prvotní načtení, user je: ", user)
     const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false)
-    const[oldPassword, setOldPassword] = useState<string>("")
-    const[newPassword, setNewPassword] = useState<string>("")
-    const[newPassword2, setNewPassword2] = useState<string>("")
+    const [oldPassword, setOldPassword] = useState<string>("")
+    const [newPassword, setNewPassword] = useState<string>("")
+    const [newPassword2, setNewPassword2] = useState<string>("")
 
     useEffect(() => {
         if (user?.username) {
-          setIsLoggedIn(true)
-          console.log("Ted se username změnil, user je ", user?.username)
+            setIsLoggedIn(true)
+            console.log("Ted se username změnil, user je ", user?.username)
         } else {
-          setIsLoggedIn(false)
+            setIsLoggedIn(false)
         }
-      }, [user])
+    }, [user])
 
-      
-    const ChangePassword = async () =>{
+
+    const ChangePassword = async () => {
 
         const res = await fetch(Urls.server + Urls.changepassword, {
             method: 'POST',
@@ -30,58 +31,61 @@ export default function Settings() {
                 'Content-Type': 'application/json',
             },
             credentials: "include",
-            body: JSON.stringify({oldPassword:oldPassword, newPassword:newPassword}),
+            body: JSON.stringify({ oldPassword: oldPassword, newPassword: newPassword }),
         })
 
-        if(!res.ok){
+        if (!res.ok) {
             alert("Něco se pokazilo. Zkontrolujte heslo nebo to zkuste později.")
         }
-        else{
+        else {
             alert("Heslo změněno úspěšně.")
         }
-    }  
+    }
 
-    const handlePasswordChangeSubmit = () =>{
+    const handlePasswordChangeSubmit = () => {
 
-        if(!(newPassword == newPassword2)){
+        if (!(newPassword == newPassword2)) {
             alert("Nové hesla se neshodují.")
             return
         }
 
         ChangePassword()
     }
-      
-    if(isLoggedIn == false){
-        return(<><h1>User must be logged in first.</h1>
-            <button onClick={()=>console.log("User context", user)}>Log user context</button>
+
+    if (isLoggedIn == false) {
+        return (<><h1>User must be logged in first.</h1>
+            <button onClick={() => console.log("User context", user)}>Log user context</button>
         </>)
-        
+
     }
-    else{
+    else {
 
-    return (    
-    <div className="settings-page">
-        <h1>Nastavení účtu</h1>
-        <h2>Změna hesla</h2>
-        <form className="password-form" method="POST">
-            <div className="form-group">
-                <label htmlFor="current-password">Aktuální heslo</label>
-                <input type="password" id="current-password" onChange={(e)=>setOldPassword(e.target.value)} name="current-password" required/>
-            </div>
-            <div className="form-group">
-                <label htmlFor="new-password">Nové heslo</label>
-                <input type="password" onChange={(e)=>setNewPassword(e.target.value)} id="new-password" name="new-password" required/>
-            </div>
-            <div className="form-group">
-                <label htmlFor="confirm-password">Potvrzení nového hesla</label>
-                <input type="password" onChange={(e)=>setNewPassword2(e.target.value)} id="confirm-password" name="confirm-password" required/>
-            </div>
-            <button type="submit" onClick={handlePasswordChangeSubmit} className="submit-btn">Změnit heslo</button>
-        </form>
-        <h2>Smazat účet</h2>
-        <button className="delete-account-btn" >Smazat účet</button>
-        
-    </div>
+        return (
+            // byl tam bug, tohle bylo nejjednoduší řešení :)
+            <div id="settings-cssfix"> 
+                <div className="settings-page">
+                    <h1>Nastavení účtu</h1>
+                    <h2>Změna hesla</h2>
+                    <form className="password-form" method="POST">
+                        <div className="form-group">
+                            <label htmlFor="current-password">Aktuální heslo</label>
+                            <input type="password" id="current-password" onChange={(e) => setOldPassword(e.target.value)} name="current-password" required />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="new-password">Nové heslo</label>
+                            <input type="password" onChange={(e) => setNewPassword(e.target.value)} id="new-password" name="new-password" required />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="confirm-password">Potvrzení nového hesla</label>
+                            <input type="password" onChange={(e) => setNewPassword2(e.target.value)} id="confirm-password" name="confirm-password" required />
+                        </div>
+                        <button type="submit" onClick={handlePasswordChangeSubmit} className="submit-btn">Změnit heslo</button>
+                    </form>
+                    <h2>Smazat účet</h2>
+                    <button className="delete-account-btn" >Smazat účet</button>
 
-    )}
+                </div>
+            </div>
+        )
+    }
 }
